7d252e6f54bafef3bd74769d8eb2f7eed69ac827
Alex Caires <alexcaires123@gmail.com>
2024-04-27 05:04:41 -0400

    -m "Initialized project. This project willbe where I test c++ functionality and create small simplistic projects

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Enter a number: "; // ask user for a number
+
+    int x{};       // define variable x to hold user input (and value-initialize it)
+    std::cin >> x; // get number from keyboard and store it in variable x
+
+    std::cout << "You entered " << x << '\n';
+    return 0;
+}
