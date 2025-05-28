dd69e545d0a007cedcab3932cceb69a35f114bf1
Alex Caires <alexcaires123@gmail.com>
2025-05-28 15:51:52 -0400

    added chapter 3 stuff

--- Chapter3/3.6/chapter3.6.mk
+++ Chapter3/3.6/chapter3.6.mk
@@ -1,10 +1,10 @@
-Test: link_chapter_3,6
+Test: link_chapter_3.6
 
 link_chapter_3: chapter_3.6.o
 	g++ chapter_3.6.o -o main.exe
 
-compile_chapter_3: link_chapter_3.6
-	g++ chapter_3,6.cpp
+compile_chapter_3: link_chapter_3
+	g++ chapter_3.6.cpp -c
 
 clean:
 	rm *.o *.exe
